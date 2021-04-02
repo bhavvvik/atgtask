4ec207e798199b98c55eab2e0d31cf87572dbb54
bhavvvik <81767404+bhavvvik@users.noreply.github.com>
2021-04-02 09:44:20 +0530

    confic change

--- config/database.php
+++ config/database.php
@@ -73,7 +73,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
+            'prefix_indexes' => false,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
